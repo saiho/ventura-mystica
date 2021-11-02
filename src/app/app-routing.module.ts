cf800b55615964e0e7550c8b229011e2360a3730
saiho <16374397+saiho@users.noreply.github.com>
2021-11-02 17:13:30 +0100

    Clean files created by the wizard.
    Add pages for new game and settings.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'new-game',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'new-game',
+    loadChildren: () => import('./new-game/new-game.module').then( m => m.NewGamePageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
   }
 ];
 
--- src/app/new-game/new-game.module.ts
+++ src/app/new-game/new-game.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { NewGamePageRoutingModule } from './new-game-routing.module';
 
-import { FolderPage } from './folder.page';
+import { NewGamePage } from './new-game.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    NewGamePageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [NewGamePage]
 })
-export class FolderPageModule {}
+export class NewGamePageModule {}
