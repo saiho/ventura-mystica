0f5705149e21b80d33c7c1c1612c23bba53eb1dc
saiho <16374397+saiho@users.noreply.github.com>
2022-02-04 19:42:20 +0100

    Refactor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,8 +5,8 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { DateIntPipe } from '../pipes/date-int.pipe';
 import { GridSelectionPageModule } from './pages/grid-selection/grid-selection.module';
-import { FactionsValidatorDirective } from './validations/input-factions-validator.directive';
-import { ScoringTilesValidatorDirective } from './validations/input-scoring-tiles-validator.directive';
+import { FactionsValidatorDirective } from './validations/factions-validator.directive';
+import { ScoringTilesValidatorDirective } from './validations/scoring-tiles-validator.directive';
 import { SelectedMinimumDirective } from './validations/selected-minimum-validator.directive';
 
 @NgModule({
